7af49301dd8e40df8f9b7d9da4a95d6f125fd75d
Vishal Kashi <dev.vishalkashi@gmail.com>
2021-12-22 16:57:48 +0530

    Add header to products section

--- components/products/Products.js
+++ components/products/Products.js
@@ -1,9 +1,12 @@
 import ProductsList from "../productsList/ProductsList";
-import { StyledProducts } from "./styledComponents";
+import { StyledProducts, StyledProductsHeader } from "./styledComponents";
 
 function Products(props) {
   return (
     <StyledProducts>
+      <StyledProductsHeader>
+        <h1>All PRODUCTS</h1>
+      </StyledProductsHeader>
       <ProductsList products={props.productsList}></ProductsList>
     </StyledProducts>
   );
--- components/products/styledComponents.js
+++ components/products/styledComponents.js
@@ -12,3 +12,14 @@ export const StyledProducts = styled.section`
     grid-gap: 0.5em;
   }
 `;
+
+export const StyledProductsHeader = styled.header`
+  h1 {
+    background-color: ${({ theme }) => theme.color.primaryAccent};
+    padding: 0.5em;
+    margin: 1em 0;
+    border: 1px solid ${({ theme }) => theme.color.primaryText};
+    text-align: center;
+    font-weight: 400;
+  }
+`;
