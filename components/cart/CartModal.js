5d6c0971c613ad7a248336b7432fda5da71e00fb
Vishal Kashi <dev.vishalkashi@gmail.com>
2021-12-22 08:41:55 +0530

    Update CartModal.js

--- components/cart/CartModal.js
+++ components/cart/CartModal.js
@@ -1,17 +1,7 @@
 import { Fragment } from "react";
 import ClientOnlyPortal from "../global/ClientOnlyPortal/ClientOnlyPortal";
 import CartOverlay from "./CartOverlay";
-import styled from "styled-components";
 import { StyledBackDrop } from "./styledComponents";
-
-// const StyledBackDrop = styled.div`
-//   background-color: rgba(0, 0, 0, 0.6);
-//   position: fixed;
-//   top: 0px;
-//   height: 100vh;
-//   width: 100vw;
-//   z-index: 100;
-// `;
 
 function CartModal(props) {
   return (
