89ea7fd9dbdb314b3b06e842076f8646b5429a93
Vishal Kashi <dev.vishalkashi@gmail.com>
2021-12-23 09:40:38 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId:
-        "591589190690-g33u6a00f5lrp938ntmpuqvsi2nsluhd.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 });
