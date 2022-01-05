d99ddd4a144f8370295f080756ff3a30812dd2ce
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-01-05 15:15:35 +0530

    Fix layout shift issue on homepage due to the collection images

--- components/collections/CategoryItems.js
+++ components/collections/CategoryItems.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import {
   StyledCategoryDetails,
   StyledCategoryItem,
@@ -8,19 +9,23 @@ function CategoryItems() {
   return (
     <>
       <StyledCategoryItem>
-        <StyledCategoryImage
+        <Image
           src="https://imgur.com/Ks6yn76.jpeg"
           alt="woman's collection"
-        ></StyledCategoryImage>
+          width={500}
+          height={707}
+        ></Image>
         <StyledCategoryDetails>
           <p>(01)</p>
           <p>MAN</p>
         </StyledCategoryDetails>
       </StyledCategoryItem>
       <StyledCategoryItem>
-        <StyledCategoryImage
+        <Image
           src="https://imgur.com/IoDYvMx.jpeg"
           alt="men's collection"
+          width={500}
+          height={707}
         />
         <StyledCategoryDetails>
           <p>(02)</p>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-
+  images: {
+    domains: ["imgur.com"],
+  },
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
   },
