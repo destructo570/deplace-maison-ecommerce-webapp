5048717918db53dd9e21f00ca16c25bf2d5d188a
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-01-09 11:15:32 +0530

    Add smooth scroll to 404 page text

--- components/notFound/NotFound.js
+++ components/notFound/NotFound.js
@@ -1,15 +1,24 @@
 import Link from "next/link";
 import { StyledNotFound } from "./styledComponents";
 import cassette from "../../assets/icons/cassette.svg";
+import Marquee from "react-easy-marquee";
 
 function NotFound() {
   return (
     <StyledNotFound>
       <img src={cassette.src} alt="" />
-      <marquee>
+      <Marquee
+        duration={60000}
+        width="100%"
+        height="100%"
+        axis="x"
+        align="center"
+        pauseOnHover={false}
+        reverse={true}
+      >
         404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404
-        404 404 404 404 404 404
-      </marquee>
+        404 404 404 404 404 404 404 404 404
+      </Marquee>
       <p>
         Wrap the cassette up and <Link href="/">come back</Link> to home.
       </p>
--- components/notFound/styledComponents.js
+++ components/notFound/styledComponents.js
@@ -1,30 +1,29 @@
 import styled from "styled-components";
 
 export const StyledNotFound = styled.section`
-  margin-top: ${({ theme }) => theme.layout.small.pageTopMargin};
-  margin-bottom: ${({ theme }) => theme.layout.small.pageTopMargin};
+  /* margin-top: 2em;
+  margin-bottom: 2em; */
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
-  height: 80vh;
+  height: 100vh;
   position: relative;
+  font-size: clamp(25rem, 40vw, 40rem);
+  font-family: "TTrailer";
 
   img {
     position: absolute;
-    top: 25vh;
+    top: 40vh;
     width: 50%;
     max-width: 400px;
-  }
-
-  marquee {
-    font-size: clamp(25rem, 40vw, 40rem);
-    font-family: "TTrailer";
+    z-index: 100;
   }
 
   p {
     font-size: 1.8rem;
     padding: 0 1em;
+    font-family: "Favorit";
   }
 
   a {
