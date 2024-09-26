5f079eda3b60fd60014f3ef12dc8532fff625152
ChemaV <quemavic@gmail.com>
2024-09-26 16:36:38 +0200

    créditos ajustados

--- components/credits/styledComponents.js
+++ components/credits/styledComponents.js
@@ -1,19 +1,25 @@
 import styled from "styled-components";
 
 export const StyledCreditsHeader = styled.div`
-  text-transform: uppercase;
+  text-transform: none;
 
   h1 {
     text-align: right;
-    font-size: clamp(7.5rem, 20vw, 8rem);
+    font-size: clamp(4.5rem, 20vw, 6rem);
     font-weight: 350;
     letter-spacing: -7px;
-    line-height: 0.8em;
+    line-height: 1em;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    font-weight: 400;
   }
 
   h3 {
     font-size: 1.5rem;
     font-weight: 400;
+    color: white;
   }
 
   ul {
@@ -21,14 +27,14 @@ export const StyledCreditsHeader = styled.div`
     margin-bottom: 10em;
     display: grid;
     grid-auto-flow: rows;
-    grid-gap: 20px;
+    grid-gap: 50px;
   }
 
   li {
     list-style: none;
     display: grid;
-    grid-template-columns: 1fr 2fr;
-    grid-gap: 20px;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 90px;
   }
 
   @media (min-width: ${({ theme }) => theme.layout.medium.size}) {
